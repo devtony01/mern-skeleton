ee696a826fb2d4d4ec5f561721024ec1d231ccc7
devtony01 <chibuezeogbuji01@gmail.com>
2023-09-25 23:37:17 +0100

    (client) configure babel and webpack, add react and material UI

---
+++ webpack.config.client.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const webpack = require('webpack')
+const CURRENT_WORKING_DIR = process.cwd()
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const config = {
+    name: "browser",
+    mode: "development",
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+    },
+    devtool: "source-map",
+    entry: {
+        main: ['webpack-hot-middleware/client', path.join(CURRENT_WORKING_DIR, 'client/main.js')]
+    },
+    output: {
+        path: path.join(CURRENT_WORKING_DIR , '/dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ]
+            }
+        ]
+    },  
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new ReactRefreshWebpackPlugin()
+    ]
+}
+
+module.exports = config
