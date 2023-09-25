f9b395b405e1587120fcb743c04893a6928b5b2d
devtony01 <chibuezeogbuji01@gmail.com>
2023-09-25 23:23:17 +0100

    auth api

--- server/express.js
+++ server/express.js
@@ -6,6 +6,7 @@ import helmet from 'helmet';
 import cors from 'cors';
 import Template from './../template';
 import userRoutes from './routes/user.routes';
+import authRoutes from './routes/auth.routes'
 
 const app = express();
 
@@ -21,5 +22,16 @@ app.get('/', (req, res) => {
 });
 
 app.use('/', userRoutes);
+app.use('/', authRoutes);
+
+// Catch unauthorised errors
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+      res.status(401).json({"error" : err.name + ": " + err.message});
+    }else if (err) {
+      res.status(400).json({"error" : err.name + ": " + err.message});
+      console.log(err);
+    }
+});
 
 export default app;
